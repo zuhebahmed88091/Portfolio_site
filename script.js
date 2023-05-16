62e9c1a9207e9bcff0fb17637229cdb33c23930a
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-16 17:42:33 +0300

    Adds validation condition, Remove required, and add CSS

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-const unionButton = document.querySelector('.union-button');
-const crossButton = document.querySelector('.cross-button');
-const hamBar = document.querySelector('.ham-bar');
-const scrollToLinks = document.querySelectorAll('.scroll');
-const form = document.getElementById('form');
-const username = document.getElementById('name');
-const email = document.getElementById('email');
+const unionButton = document.querySelector(".union-button");
+const crossButton = document.querySelector(".cross-button");
+const hamBar = document.querySelector(".ham-bar");
+const scrollToLinks = document.querySelectorAll(".scroll");
+const form = document.getElementById("form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
 
 function showMobileMenu() {
-  hamBar.style.display = 'block';
+  hamBar.style.display = "block";
 }
 
 function hideMobileMenu() {
-  hamBar.style.display = 'none';
+  hamBar.style.display = "none";
 }
 function scrollSection(sectionId) {
   const section = document.getElementById(sectionId);
   if (section) {
-    section.scrollIntoView({ behavior: 'smooth' });
+    section.scrollIntoView({ behavior: "smooth" });
   }
 }
 scrollToLinks.forEach((link) => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
-    const sectionId = link.getAttribute('href');
+    const sectionId = link.getAttribute("href");
     scrollSection(sectionId);
     hideMobileMenu();
   });
 });
-unionButton.addEventListener('click', showMobileMenu);
+unionButton.addEventListener("click", showMobileMenu);
 
-crossButton.addEventListener('click', hideMobileMenu);
+crossButton.addEventListener("click", hideMobileMenu);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('form');
-  const username = document.getElementById('name');
-  const email = document.getElementById('email');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  const username = document.getElementById("name");
+  const email = document.getElementById("email");
 
-  form.addEventListener('submit', (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
     validateFormInputs();
   });
 
   function setError(elm, msg) {
     const inputControl = elm.parentElement;
-    const errorDiv = inputControl.querySelector('.error');
+    const errorDiv = inputControl.querySelector(".error");
     errorDiv.innerText = msg;
-    inputControl.classList.add('error');
+    inputControl.classList.add("error");
   }
 
   function clearError(elm) {
     const inputControl = elm.parentElement;
-    const errorDiv = inputControl.querySelector('.error');
-    errorDiv.innerText = '';
-    inputControl.classList.remove('error');
+    const errorDiv = inputControl.querySelector(".error");
+    errorDiv.innerText = "";
+    inputControl.classList.remove("error");
   }
 
   function validateFormInputs() {
@@ -61,5 +61,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     clearError(username);
     clearError(email);
+
+    if (usernameValue === "") {
+      setError(username, "Username is required");
+    }
+    if (emailValue === "") {
+      setError(email, "Email is required");
+    }
   }
 });
