e6d20c0c58f6df2ab98b5b6be81a2f0500c2f940
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-05-16 20:50:47 +0600

    Fix linters errors

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-const unionButton = document.querySelector(".union-button");
-const crossButton = document.querySelector(".cross-button");
-const hamBar = document.querySelector(".ham-bar");
-const scrollToLinks = document.querySelectorAll(".scroll");
-const form = document.getElementById("form");
-const username = document.getElementById("name");
-const email = document.getElementById("email");
+const unionButton = document.querySelector('.union-button');
+const crossButton = document.querySelector('.cross-button');
+const hamBar = document.querySelector('.ham-bar');
+const scrollToLinks = document.querySelectorAll('.scroll');
 
 function showMobileMenu() {
-  hamBar.style.display = "block";
+  hamBar.style.display = 'block';
 }
 
 function hideMobileMenu() {
-  hamBar.style.display = "none";
+  hamBar.style.display = 'none';
 }
 function scrollSection(sectionId) {
   const section = document.getElementById(sectionId);
   if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
+    section.scrollIntoView({ behavior: 'smooth' });
   }
 }
 scrollToLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
+  link.addEventListener('click', (event) => {
     event.preventDefault();
-    const sectionId = link.getAttribute("href");
+    const sectionId = link.getAttribute('href');
     scrollSection(sectionId);
     hideMobileMenu();
   });
 });
-unionButton.addEventListener("click", showMobileMenu);
+unionButton.addEventListener('click', showMobileMenu);
 
-crossButton.addEventListener("click", hideMobileMenu);
+crossButton.addEventListener('click', hideMobileMenu);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("form");
-  const username = document.getElementById("name");
-  const email = document.getElementById("email");
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    validateFormInputs();
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  const username = document.getElementById('name');
+  const email = document.getElementById('email');
 
   function setError(elm, msg) {
     const inputControl = elm.parentElement;
-    const errorDiv = inputControl.querySelector(".error");
+    const errorDiv = inputControl.querySelector('.error');
     errorDiv.innerText = msg;
-    inputControl.classList.add("error");
+    inputControl.classList.add('error');
   }
 
   function clearError(elm) {
     const inputControl = elm.parentElement;
-    const errorDiv = inputControl.querySelector(".error");
-    errorDiv.innerText = "";
-    inputControl.classList.remove("error");
+    const errorDiv = inputControl.querySelector('.error');
+    errorDiv.innerText = '';
+    inputControl.classList.remove('error');
   }
 
   function validateFormInputs() {
@@ -62,11 +54,16 @@ document.addEventListener("DOMContentLoaded", () => {
     clearError(username);
     clearError(email);
 
-    if (usernameValue === "") {
-      setError(username, "Username is required");
+    if (usernameValue === '') {
+      setError(username, 'Username is required');
     }
-    if (emailValue === "") {
-      setError(email, "Email is required");
+    if (emailValue === '') {
+      setError(email, 'Email is required');
     }
   }
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validateFormInputs();
+  });
 });
