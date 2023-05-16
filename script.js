fed50ada3ffc3072ff412d3aa26bbadd4ab84849
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-05-16 20:27:22 +0600

    Add form validation

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const unionButton = document.querySelector('.union-button');
 const crossButton = document.querySelector('.cross-button');
 const hamBar = document.querySelector('.ham-bar');
 const scrollToLinks = document.querySelectorAll('.scroll');
+const form = document.getElementById('form');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
 
 function showMobileMenu() {
   hamBar.style.display = 'block';
@@ -27,3 +30,43 @@ scrollToLinks.forEach((link) => {
 unionButton.addEventListener('click', showMobileMenu);
 
 crossButton.addEventListener('click', hideMobileMenu);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('form');
+  const username = document.getElementById('name');
+  const email = document.getElementById('email');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validateFormInputs();
+  });
+
+  function setError(elm, msg) {
+    const inputControl = elm.parentElement;
+    const errorDiv = inputControl.querySelector('.error');
+    errorDiv.innerText = msg;
+    inputControl.classList.add('error');
+  }
+
+  function clearError(elm) {
+    const inputControl = elm.parentElement;
+    const errorDiv = inputControl.querySelector('.error');
+    errorDiv.innerText = '';
+    inputControl.classList.remove('error');
+  }
+
+  function validateFormInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+
+    clearError(username);
+    clearError(email);
+
+    if (usernameValue === '') {
+      setError(username, 'Username is required');
+    }
+    if (emailValue === '') {
+      setError(email, 'Email is required');
+    }
+  }
+});
